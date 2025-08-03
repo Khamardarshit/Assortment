6ba71ef905104a67391d8afac4c920c8a212e321
Khamardarshit <khamarrdarshir@gmail.com>
2025-08-03 20:12:21 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     int size;
 
-    // Input for square matrix
+
     cout << "Enter the array's row & column size: ";
     cin >> size;
 
     int a[size][size], transpose[size][size];
 
-    // Input elements
+
     cout << "Enter array's elements:\n";
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
@@ -19,20 +19,20 @@ int main() {
         }
     }
 
-    // Calculate transpose
+
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             transpose[j][i] = a[i][j];
         }
     }
 
-    // Output with space between elements
+    
     cout << "\nThe transpose matrix of an array:\n";
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            cout << transpose[i][j] << " ";  // Add space between columns
+            cout << transpose[i][j] << " ";  
         }
-        cout << endl;  // New line after each row
+        cout << endl;  
     }
 
     return 0;
